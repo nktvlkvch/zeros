eb4f815060c516f18efd4d59456d7c8345cd9c14
Pozharitskiy <pozharitskij.denis@gmail.com>
2018-09-27 00:23:42 +0300

    zeroes
    EASY THAN UR THINKIN

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function getZerosCount(number) {
+	//1 2 3 4 5 6 7 8 9     2,5 4,5 6,5 8,5 
   // your implementation
+let result=0;
+while (number>0) {
+	number=Math.floor(number/5);
+	result+=number;
 }
+return result;
+}
+
