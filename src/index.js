28018ccc2825bac6a7fe760f3ee4a0bb9befc964
Nikita Valkevich <n.valkevich@icloud.com>
2018-09-30 23:59:09 +0300

    sd

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports = function getZerosCount(number) {
 	//1 2 3 4 5 6 7 8 9     2,5 4,5 6,5 8,5 
   // your implementation
-let result=0;
+var result=0;
 while (number>0) {
 	number=Math.floor(number/5);
 	result+=number;
